4867c2dd47211c88757422ea3237e3a8dc414da8
Onkar <onkarshinde@users.sourceforge.net>
2009-12-26 23:47:25 +0530

    Fix a stupid mistake

---
+++ autoclean.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -rf aclocal.m4 config.h.in configure Makefile.in data/Makefile.in \
+                doc/Makefile.in macros/Makefile.in pixmaps/Makefile.in \
+                src/Makefile.in
